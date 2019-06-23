539141c4b2dbb93504d58a23459279d49391d7bc
zhongyuehui <898542119@qq.com>
2019-06-23 13:53:01 +0800

    Site updated: 2019-06-23 13:52:57

--- js/insight.js
+++ js/insight.js
@@ -2,7 +2,7 @@
  * Insight search plugin
  * @author PPOffice { @link https://github.com/ppoffice }
  */
-(function ($, CONFIG) {
+(function($, CONFIG) {
     var $main = $('.ins-search');
     var $input = $main.find('.ins-search-input');
     var $wrapper = $main.find('.ins-section-wrapper');
@@ -10,12 +10,12 @@
     $main.parent().remove('.ins-search');
     $('body').append($main);
 
-    function section (title) {
+    function section(title) {
         return $('<section>').addClass('ins-section')
             .append($('<header>').addClass('ins-section-header').text(title));
     }
 
-    function searchItem (icon, title, slug, preview, url) {
+    function searchItem(icon, title, slug, preview, url) {
         return $('<div>').addClass('ins-selectable').addClass('ins-search-item')
             .append($('<header>').append($('<i>').addClass('fa').addClass('fa-' + icon))
                 .append($('<span>').addClass('ins-title').text(title != null && title !== '' ? title : CONFIG.TRANSLATION['UNTITLED']))
@@ -24,7 +24,7 @@
             .attr('data-url', url);
     }
 
-    function sectionFactory (type, array) {
+    function sectionFactory(type, array) {
         var sectionTitle;
         var $searchItems;
         if (array.length === 0) return null;
@@ -32,14 +32,14 @@
         switch (type) {
             case 'POSTS':
             case 'PAGES':
-                $searchItems = array.map(function (item) {
+                $searchItems = array.map(function(item) {
                     // Use config.root instead of permalink to fix url issue
                     return searchItem('file', item.title, null, item.text.slice(0, 150), item.link);
                 });
                 break;
             case 'CATEGORIES':
             case 'TAGS':
-                $searchItems = array.map(function (item) {
+                $searchItems = array.map(function(item) {
                     return searchItem(type === 'CATEGORIES' ? 'folder' : 'tag', item.name, item.slug, null, item.link);
                 });
                 break;
@@ -49,10 +49,10 @@
         return section(sectionTitle).append($searchItems);
     }
 
-    function parseKeywords (keywords) {
-        return keywords.split(' ').filter(function (keyword) {
+    function parseKeywords(keywords) {
+        return keywords.split(' ').filter(function(keyword) {
             return !!keyword;
-        }).map(function (keyword) {
+        }).map(function(keyword) {
             return keyword.toUpperCase();
         });
     }
@@ -62,10 +62,10 @@
      * @param Object            obj     Object to be weighted
      * @param Array<String>     fields  Object's fields to find matches
      */
-    function filter (keywords, obj, fields) {
+    function filter(keywords, obj, fields) {
         var keywordArray = parseKeywords(keywords);
-        var containKeywords = keywordArray.filter(function (keyword) {
-            var containFields = fields.filter(function (field) {
+        var containKeywords = keywordArray.filter(function(keyword) {
+            var containFields = fields.filter(function(field) {
                 if (!obj.hasOwnProperty(field))
                     return false;
                 if (obj[field].toUpperCase().indexOf(keyword) > -1)
@@ -78,18 +78,18 @@
         return containKeywords.length === keywordArray.length;
     }
 
-    function filterFactory (keywords) {
+    function filterFactory(keywords) {
         return {
-            POST: function (obj) {
+            POST: function(obj) {
                 return filter(keywords, obj, ['title', 'text']);
             },
-            PAGE: function (obj) {
+            PAGE: function(obj) {
                 return filter(keywords, obj, ['title', 'text']);
             },
-            CATEGORY: function (obj) {
+            CATEGORY: function(obj) {
                 return filter(keywords, obj, ['name', 'slug']);
             },
-            TAG: function (obj) {
+            TAG: function(obj) {
                 return filter(keywords, obj, ['name', 'slug']);
             }
         };
@@ -101,11 +101,11 @@
      * @param Array<String>     fields  Object's fields to find matches
      * @param Array<Integer>    weights Weight of every field
      */
-    function weight (keywords, obj, fields, weights) {
+    function weight(keywords, obj, fields, weights) {
         var value = 0;
-        parseKeywords(keywords).forEach(function (keyword) {
+        parseKeywords(keywords).forEach(function(keyword) {
             var pattern = new RegExp(keyword, 'img'); // Global, Multi-line, Case-insensitive
-            fields.forEach(function (field, index) {
+            fields.forEach(function(field, index) {
                 if (obj.hasOwnProperty(field)) {
                     var matches = obj[field].match(pattern);
                     value += matches ? matches.length * weights[index] : 0;
@@ -115,24 +115,24 @@
         return value;
     }
 
-    function weightFactory (keywords) {
+    function weightFactory(keywords) {
         return {
-            POST: function (obj) {
+            POST: function(obj) {
                 return weight(keywords, obj, ['title', 'text'], [3, 1]);
             },
-            PAGE: function (obj) {
+            PAGE: function(obj) {
                 return weight(keywords, obj, ['title', 'text'], [3, 1]);
             },
-            CATEGORY: function (obj) {
+            CATEGORY: function(obj) {
                 return weight(keywords, obj, ['name', 'slug'], [1, 1]);
             },
-            TAG: function (obj) {
+            TAG: function(obj) {
                 return weight(keywords, obj, ['name', 'slug'], [1, 1]);
             }
         };
     }
 
-    function search (json, keywords) {
+    function search(json, keywords) {
         var WEIGHTS = weightFactory(keywords);
         var FILTERS = filterFactory(keywords);
         var posts = json.posts;
@@ -140,21 +140,21 @@
         var tags = json.tags;
         var categories = json.categories;
         return {
-            posts: posts.filter(FILTERS.POST).sort(function (a, b) { return WEIGHTS.POST(b) - WEIGHTS.POST(a); }).slice(0, 5),
-            pages: pages.filter(FILTERS.PAGE).sort(function (a, b) { return WEIGHTS.PAGE(b) - WEIGHTS.PAGE(a); }).slice(0, 5),
-            categories: categories.filter(FILTERS.CATEGORY).sort(function (a, b) { return WEIGHTS.CATEGORY(b) - WEIGHTS.CATEGORY(a); }).slice(0, 5),
-            tags: tags.filter(FILTERS.TAG).sort(function (a, b) { return WEIGHTS.TAG(b) - WEIGHTS.TAG(a); }).slice(0, 5)
+            posts: posts.filter(FILTERS.POST).sort(function(a, b) { return WEIGHTS.POST(b) - WEIGHTS.POST(a); }).slice(0, 5),
+            pages: pages.filter(FILTERS.PAGE).sort(function(a, b) { return WEIGHTS.PAGE(b) - WEIGHTS.PAGE(a); }).slice(0, 5),
+            categories: categories.filter(FILTERS.CATEGORY).sort(function(a, b) { return WEIGHTS.CATEGORY(b) - WEIGHTS.CATEGORY(a); }).slice(0, 5),
+            tags: tags.filter(FILTERS.TAG).sort(function(a, b) { return WEIGHTS.TAG(b) - WEIGHTS.TAG(a); }).slice(0, 5)
         };
     }
 
-    function searchResultToDOM (searchResult) {
+    function searchResultToDOM(searchResult) {
         $container.empty();
         for (var key in searchResult) {
             $container.append(sectionFactory(key.toUpperCase(), searchResult[key]));
         }
     }
 
-    function scrollTo ($item) {
+    function scrollTo($item) {
         if ($item.length === 0) return;
         var wrapperHeight = $wrapper[0].clientHeight;
         var itemTop = $item.position().top - $wrapper.scrollTop();
@@ -167,10 +167,10 @@
         }
     }
 
-    function selectItemByDiff (value) {
+    function selectItemByDiff(value) {
         var $items = $.makeArray($container.find('.ins-selectable'));
         var prevPosition = -1;
-        $items.forEach(function (item, index) {
+        $items.forEach(function(item, index) {
             if ($(item).hasClass('active')) {
                 prevPosition = index;
                 return;
@@ -182,17 +182,17 @@
         scrollTo($($items[nextPosition]));
     }
 
-    function gotoLink ($item) {
+    function gotoLink($item) {
         if ($item && $item.length) {
             location.href = $item.attr('data-url');
         }
     }
 
-    $.getJSON(CONFIG.CONTENT_URL, function (json) {
+    $.getJSON(CONFIG.CONTENT_URL, function(json) {
         if (location.hash.trim() === '#ins-search') {
             $main.addClass('show');
         }
-        $input.on('input', function () {
+        $input.on('input', function() {
             var keywords = $(this).val();
             searchResultToDOM(search(json, keywords));
         });
@@ -200,40 +200,45 @@
     });
 
     var touch = false;
-    $(document).on('click focus', '.navbar-main .search', function () {
+    $(document).on('click focus', '.navbar-main .search', function() {
         $main.addClass('show');
         $main.find('.ins-search-input').focus();
-    }).on('click touchend', '.ins-search-item', function (e) {
+    }).on('click touchend', '.ins-search-item', function(e) {
         if (e.type !== 'click' && !touch) {
             return;
         }
         gotoLink($(this));
         touch = false;
-    }).on('click touchend', '.ins-close', function (e) {
+    }).on('click touchend', '.ins-close', function(e) {
         if (e.type !== 'click' && !touch) {
             return;
         }
         $('.navbar-main').css('pointer-events', 'none');
-        setTimeout(function(){
+        setTimeout(function() {
             $('.navbar-main').css('pointer-events', 'auto');
         }, 400);
         $main.removeClass('show');
         touch = false;
-    }).on('keydown', function (e) {
+    }).on('keydown', function(e) {
         if (!$main.hasClass('show')) return;
         switch (e.keyCode) {
             case 27: // ESC
-                $main.removeClass('show'); break;
+                $main.removeClass('show');
+                break;
             case 38: // UP
-                selectItemByDiff(-1); break;
+                selectItemByDiff(-1);
+                break;
             case 40: // DOWN
-                selectItemByDiff(1); break;
+                selectItemByDiff(1);
+                break;
             case 13: //ENTER
-                gotoLink($container.find('.ins-selectable.active').eq(0)); break;
+                gotoLink($container.find('.ins-selectable.active').eq(0));
+                break;
         }
-    }).on('touchstart', function (e) {
+    }).on('touchstart', function(e) {
         touch = true;
-    }).on('touchmove', function (e) {
+    }).on('touchmove', function(e) {
         touch = false;
     });
+    $('p').find('code').eq(1).css('backgroundColor', '#F7F7F7')
 })(jQuery, window.INSIGHT_CONFIG);
